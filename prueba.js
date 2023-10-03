6fde4c223817d54565c6a29e8144a79afa501e44
Mario <maalnu93@hotmail.com>
2023-10-03 10:47:07 +0200

    PruebaNoEvaluable_Mario

---
+++ prueba.js
@@ -0,0 +1,18 @@
+//Mario Alfonso Nuñez
+while(true){
+let num = prompt("Dame un n")
+num=parseFloat(num)
+if (num>0){
+    alert("El n es positivo")
+    console.log("El n es positivo")
+}
+else if (num==0){
+    alert("El n es 0")
+    console.log("El n es 0")
+}
+else{
+    alert("El n es negativo")
+    console.log("El n es negativo")
+
+}
+}
